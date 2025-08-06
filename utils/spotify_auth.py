f081d2f7cd15cdd6981b511c7a8a9a1cf586c4c4
nickhellmer <nhellmer25@gmail.com>
2025-08-06 14:48:08 -0500

    updated datetime call 2

--- utils/spotify_auth.py
+++ utils/spotify_auth.py
@@ -64,13 +64,15 @@ def get_user_profile(access_token):
 
 def refresh_token_if_needed(session_user):
     expires_at = session_user['expires_at']
+    
+    # Convert to UNIX timestamp if it's a datetime object
     if isinstance(expires_at, datetime):
         expires_at = int(expires_at.timestamp())
     
     token_info = {
         'access_token': session_user['access_token'],
         'refresh_token': session_user['refresh_token'],
-        'expires_at': session_user['expires_at']
+        'expires_at': expires_at
     }
 
     sp_oauth = SpotifyOAuth(
